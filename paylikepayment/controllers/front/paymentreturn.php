b2497ce369a9449942d2c090a46db85b4f746bdf
Ionut Calara <ionut.calara@gmail.com>
2020-08-04 18:26:04 +0300

    Update version

--- paylikepayment/controllers/front/paymentreturn.php
+++ paylikepayment/controllers/front/paymentreturn.php
@@ -4,7 +4,7 @@
  * @author    DerikonDevelopment <ionut@derikon.com>
  * @copyright Copyright (c) permanent, DerikonDevelopment
  * @license   Addons PrestaShop license limitation
- * @version   1.0.7
+ * @version   1.0.8
  * @link      http://www.derikon.com/
  *
  */
