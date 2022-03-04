6566af36dcd7be7f4a88f72324f50b1ebdc64498
LucianDex <remindfwd@gmail.com>
2022-03-04 14:35:48 +0200

    Update quick_test.js

--- cypress/integration/quick_test.js
+++ cypress/integration/quick_test.js
@@ -9,7 +9,12 @@ describe('paylike plugin quick test', () => {
      * Login into admin and frontend to store cookies.
      */
     before(() => {
-        cy.goToPage(TestMethods.StoreUrl + '/index.php?route=account/login');
+        /**
+         * Note
+         * "/index.php?controller=authentication&back=my-account" may be an old uri
+         * On Prestashop newer versions is redirect to "/login?&back=my-account"
+         */
+        cy.goToPage(TestMethods.StoreUrl + '/index.php?controller=authentication&back=my-account');
         TestMethods.loginIntoClientAccount();
         cy.goToPage(Cypress.env('ENV_ADMIN_URL'));
         TestMethods.loginIntoAdminBackend();
